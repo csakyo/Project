33dc7297599dcd48c9e73e16c2d700732b487af6
csakyo <c.sakyou@gmail.com>
2023-04-23 18:47:52 +0900

    Feat: readonly

---
+++ TypeScript/section2/dist/readonly.js
@@ -0,0 +1,6 @@
+const memberR = {
+    name: '田中',
+    age: 22
+};
+// memberR.name = '松井'
+//# sourceMappingURL=readonly.js.map
---
+++ TypeScript/section2/src/readonly.ts
@@ -0,0 +1,11 @@
+type MemberTypeR = {
+    readonly name: string,
+    age: number
+}
+
+const memberR: MemberTypeR = {
+    name: '田中',
+    age: 22
+}
+
+// memberR.name = '松井'
