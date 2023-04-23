e67de6044fbe2c9867d953012b7c1a9cdfa39592
csakyo <c.sakyou@gmail.com>
2023-04-23 15:07:48 +0900

    Fate: Argument handling

--- TypeScript/section1/src/function.ts
+++ TypeScript/section1/src/function.ts
@@ -10,3 +10,28 @@ const arrowFuncTest = (str: string, int: number) : void => {
 }
 
 arrowFuncTest('aaa', 123)
+
+
+//引数省略
+const funcTestQ = (str: string, int? : number) : void => {
+    console.log(str, int)
+}
+
+funcTestQ('Qマーク')
+
+// デフォルト値指定
+const funcTestD = (int? : number, str: string = 'aaa') : void => {
+    console.log(str, int)
+}
+
+funcTestD(123)
+
+
+//可変長パラメータ
+const eachNumber = (...items: number[]) : void => {
+    for(const item of items) {
+        console.log(item)
+    }
+}
+
+eachNumber(1,2,3)
