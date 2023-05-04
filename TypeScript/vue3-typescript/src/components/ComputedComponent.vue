90cd74709b5d71f328bf1223c431e2f5903ff7a1
csakyo <c.sakyou@gmail.com>
2023-05-04 13:16:20 +0900

    Feat: ve3TypeScript

---
+++ TypeScript/vue3-typescript/src/components/ComputedComponent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+const amount = ref<number>(0)
+
+const subTotal = computed(() => 3000 * amount.value)
+</script>
+
+<template>
+    <h2>Computed</h2>
+    単価: 3000<br>
+    個数: <input type="number" v-model="amount">
+    小計: {{ subTotal }}
+</template>
---
+++ TypeScript/vue3-typescript/src/components/ReactiveComponent.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+type User = {
+    id: number,
+    name: string
+}
+
+const user1: User = {
+    id: 1,
+    name: '田中'
+}
+
+const user2 = reactive({} as User)
+user2.id = 1
+user2.name = '佐藤'
+
+</script>
+
+<template>
+    <h2>reactive</h2>
+    {{ user1.name }}
+    {{ user2.name }}
+</template>
