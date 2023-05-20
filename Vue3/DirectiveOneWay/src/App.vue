06db0d308406edfc02b3ab31b76082dae861dbfc
csakyo <c.sakyou@gmail.com>
2023-05-20 09:42:27 +0900

    Feat: direvtive

---
+++ Vue3/DirectiveOneWay/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const inputNameBind = ref('aaa')
+const inputNameOn = ref('bbb')
+
+
+const onInputName = (e) => {
+  const element = e.target.value;
+  inputNameOn.value = element
+}
+</script>
+
+<template>
+	<section>
+		<input type="text" v-bind:value="inputNameBind">
+	<br>
+		<input type="text" v-on:input="onInputName">
+		<p>{{inputNameOn}}</p>
+	</section>
+</template>
