bf76ad848b02dd572c7935b98ba96f2ce1f7a33d
csakyo <c.sakyou@gmail.com>
2023-06-03 16:06:19 +0900

    Feat: props

---
+++ Vue3/components-props-basics/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lag="ts">
+import OneInfo from "./components/OneInfo.vue"
+</script>
+
+<template>
+  <h1>Props</h1>
+  <section>
+    <h2>TITLE</h2>
+    <OneInfo title="Propsの基礎" content="コンポーネントに値を渡す" />
+  </section>
+</template>
+
+<style>
+  section {
+    border: blue 1px solid;
+    margin: 10px;
+  }
+</style>
