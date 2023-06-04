a3ad2ced0ad73a566ab1ddafb5043dfce71b12df
csakyo <c.sakyou@gmail.com>
2023-06-04 10:54:31 +0900

    Feat: v-for and props work

--- Vue3/components-props-basics/src/App.vue
+++ Vue3/components-props-basics/src/App.vue
@@ -1,12 +1,36 @@
-<script setup lag="ts">
-import OneInfo from "./components/OneInfo.vue"
+<script setup lang="ts">
+import {ref} from "vue";
+import OneInfo from "./components/OneInfo.vue";
+
+const weatherListInit: Weather[] = [
+  { id: 1,
+    title: '今日の天気',
+    content: '今日は一日中晴でしょう'
+  },
+  { id: 2,
+    title: '明日の天気',
+    content: '明日は一日中曇りでしょう'
+  },
+  { id: 3,
+    title: '明後日の天気',
+    content: '明後日は一日中雨でしょう'
+  },
+]
+const weatherList = ref(weatherListInit);
+interface Weather {
+  id: number;
+  title: string;
+  content: string;
+}
 </script>
 
 <template>
-  <h1>Props</h1>
   <section>
     <h2>TITLE</h2>
-    <OneInfo title="Propsの基礎" content="コンポーネントに値を渡す" />
+    <OneInfo v-for="weather in weatherList" 
+    :key="weather.id"
+    :title="weather.title"
+    :content="weather.content" />
   </section>
 </template>
 
