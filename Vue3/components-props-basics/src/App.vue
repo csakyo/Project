99e35f18ddcbb2271fb241d30b2776bd8da03524
csakyo <c.sakyou@gmail.com>
2023-06-12 21:12:09 +0900

    Feat: props

--- Vue3/components-props-basics/src/App.vue
+++ Vue3/components-props-basics/src/App.vue
@@ -1,22 +1,31 @@
 <script setup lang="ts">
 import {ref} from "vue";
-import OneInfo from "./components/OneInfo.vue";
+import OneMember from "./components/OneMember.vue";
 
-const weatherListInit = [
-  {id: 1, title:"今日の天気", content:"晴れ"},
-  {id: 2, title:"明日の天気", content:"雨"},
-  {id: 3, title:"明後日の天気", content:"曇り"},
+// const weatherListInit = [
+//   {id: 1, title:"今日の天気", content:"晴れ"},
+//   {id: 2, title:"明日の天気", content:"雨"},
+//   {id: 3, title:"明後日の天気", content:"曇り"},
+// ]
+// const weatherList = ref(weatherListInit)
+
+const memberListInit = [
+  {id:1, name:"田中太郎", mail:"aaa@gmail.com", point: 1, note:"会員メンバー" },
+  {id:1, name:"佐藤二郎", mail:"bbb@gmail.com", point: 3, note:"非会員メンバー" },
+  {id:1, name:"鈴木三郎", mail:"ccc@gmail.com", point: 6, },
 ]
-const weatherList = ref(weatherListInit)
+const memberList = ref(memberListInit)
 </script>
 
 <template>
   <section>
     <h2>TITLE</h2>
-    <OneInfo v-for="weather in weatherList"
-    :key="weather.id"
-    :title="weather.title"
-    :content="weather.content"/>
+    <OneMember v-for="member in memberList"
+    :key="member.id"
+    :name="member.name"
+    :mail="member.mail"
+    :point="member.point"
+    :note="member.note" />
   </section>
 </template>
 
---
+++ Vue3/components-props-basics/src/components/OneMember.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import {ref, computed} from "vue"
+
+interface Props {
+    id: number;
+    name: string;
+    mail: string;
+    point: number;
+    note: string;
+}
+const props = withDefaults(
+    defineProps<Props>(),
+    {note: "--"}
+);
+
+// const aboutNote = computed(() => {
+//     let note = props.note;
+//     if (note == undefined) {
+//         note = "--";
+//     }
+//     return note
+// })
+
+const point = ref(props.point)
+const pointUp = () => {
+    point.value++
+}
+</script>
+
+<template>
+    <section class="box">
+        <h4>{{ name }}さんの情報</h4>
+        <p>メールアドレス：{{ mail }}</p>
+        <p>ポイント：{{ point }}</p>
+        <p>備考：{{ note }}</p>
+        <button @click="pointUp">ポイント加算</button>
+    </section>
+</template>
+
+<style scoped>
+.box {
+ border: green 1px dashed;
+ margin: 10px;
+}
+</style>
