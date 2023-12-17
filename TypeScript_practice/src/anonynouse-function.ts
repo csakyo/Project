05e2e8979981ba03c330f56cdebdcb03691cd2ba
csakyo <c.sakyou@gmail.com>
2023-12-17 16:41:52 +0900

    feature/function

---
+++ TypeScript_practice/src/anonynouse-function.ts
@@ -0,0 +1,10 @@
+export {};
+
+let bmi: (height: number, weight: number) => number = function bmi(
+  height: number,
+  weight: number
+): number {
+  return weight / (height * height);
+};
+
+console.log(bmi(1.69, 51));
