8d7a066b90baabb2f237daeb9709e88ccbcf90f2
csakyo <c.sakyou@gmail.com>
2024-01-20 20:55:13 +0900

    feat: components

---
+++ React_basic/04_react_basic/src/070_component_nest/components/Child.js
@@ -0,0 +1,13 @@
+import "./Child.css";
+import { List } from "./List.js";
+
+const Example = () => {
+  return (
+    <div className="component">
+      <h3>Hello Component</h3>
+      <List />
+    </div>
+  );
+};
+
+export default Example;
