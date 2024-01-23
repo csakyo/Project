72e583ec292c02178e6239666ed3f8411e0bf92f
csakyo <c.sakyou@gmail.com>
2024-01-23 23:17:29 +0900

    feat: static

--- TypeScript_practice/src/static-member.ts
+++ TypeScript_practice/src/static-member.ts
@@ -4,6 +4,10 @@ class Me {
   static isProgrammer: boolean = true;
   static firstName: string = "Atsushi";
   static lastName: string = "Ishida";
+
+  static work() {
+    return `Hey, guys! This is ${this.firstName}! Are you intersted in TypeScript?`;
+  }
 }
 
 let me = new Me();
