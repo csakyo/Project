5333a352bcd0764e440319f1511385a40c5438b2
csakyo <c.sakyou@gmail.com>
2024-01-26 23:21:54 +0900

    feat: namespace

--- TypeScript_practice/src/namespace.ts
+++ TypeScript_practice/src/namespace.ts
@@ -1,5 +1,19 @@
 export {};
 
-class Person {
-  constructor(public name: string) {}
+namespace Japanese {
+  export class Person {
+    constructor(public name: string) {}
+  }
 }
+
+namespace English {
+  export class Person {
+    constructor(
+      public firstName: string,
+      public middleName: string,
+      public lastName: string
+    ) {}
+  }
+}
+
+const me = new Japanese.Person("taro");
