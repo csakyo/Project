d4a7565a2f515a4608007edf407cbe15cf862f8c
csakyo <c.sakyou@gmail.com>
2024-02-02 21:58:51 +0900

    feat: inheritance

--- TypeScript_practice/src/inheritance.ts
+++ TypeScript_practice/src/inheritance.ts
@@ -9,16 +9,19 @@ class Animal {
 }
 
 class Lion extends Animal {
-  constructor(public name: string) {
+  public speed: number;
+  constructor(name: string, speed: number) {
     super(name);
+
+    this.speed = speed;
   }
   run(): string {
     return "I can run 89km.";
   }
 }
 
-let animal = new Animal();
+let animal = new Animal("Taro");
 console.log(animal.run());
 
-let lion = new Lion();
+let lion = new Lion("Simba", 90);
 console.log(lion.run());
