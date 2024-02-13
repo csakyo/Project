906b9c3e3c3ddee0187574ff4ac84495e4a90d7f
csakyo <c.sakyou@gmail.com>
2024-02-13 22:20:08 +0900

    feat: useState

---
+++ React_basic/05_state_and_event/src/050_prev_state/Example.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+const Example = () => {
+  const [count, setCount] = useState(0);
+  const countUp = () => {
+    setCount(count + 1);
+  };
+  return <></>;
+};
