34a86d66b084934608dc9febd154f63a54aab2f9
csakyo <c.sakyou@gmail.com>
2024-02-24 11:42:57 +0900

    feat: useState

--- React_basic/05_state_and_event/src/070_state_and_component/Example.js
+++ React_basic/05_state_and_event/src/070_state_and_component/Example.js
@@ -1,26 +1,31 @@
 import { useState } from "react";
 
-// const Example = () => {
-//   return (
-//     <>
-//       <Count />
-//       <Count />
-//     </>
-//   );
-// };
-
 const Example = () => {
+  const [toggle, setToggle] = useState(0);
+  const toggleComponent = () => {
+    setToggle((prev) => !prev);
+  };
+  return (
+    <>
+      <button onClick={toggleComponent}>toggle</button>
+      {toggle ? <Count key="A" title="A" /> : <Count key="B" title="B" />}
+    </>
+  );
+};
+
+const Count = ({ title }) => {
   const [count, setCount] = useState(0);
   const countUp = () => {
     setCount(count + 1);
-    setCount((prevstate) => prevstate + 1);
   };
   const countDown = () => {
     setCount(count - 1);
   };
   return (
     <>
-      <p>現在のカウント数: {count}</p>
+      <p>
+        {title}: {count}
+      </p>
       <button onClick={countUp}>+</button>
       <button onClick={countDown}>-</button>
     </>
