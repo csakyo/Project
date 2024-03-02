60752cabf2bbca89bcbdf5e11ec69e0100616080
csakyo <c.sakyou@gmail.com>
2024-03-02 12:34:04 +0900

    feat: generics

--- TypeScript_practice/src/generics.ts
+++ TypeScript_practice/src/generics.ts
@@ -5,3 +5,15 @@ const echo = <T>(arg: T): T => {
 };
 
 console.log(echo<number>(100));
+
+class Mirror<T> {
+  constructor(public value: T) {}
+
+  echo(): T {
+    return this.value;
+  }
+}
+
+console.log(new Mirror(233).echo());
+console.log(new Mirror("T").echo());
+console.log(new Mirror(true).echo());
