030fca827756b53397f295d68c677c1d2f2cbc2e
csakyo <c.sakyou@gmail.com>
2024-03-04 20:58:13 +0900

    feat: generics

--- TypeScript_practice/src/generics.ts
+++ TypeScript_practice/src/generics.ts
@@ -8,6 +8,8 @@ const echo2 = function <T>(arg: T): T {
   return arg;
 };
 
+console.log(echo<number>(100));
+
 const echo3 = {
   echo<T>(arg: T): T {
     return arg;
@@ -15,6 +17,12 @@ const echo3 = {
 };
 
 console.log(echo3.echo<number>(100));
+
+const echo4 = <T>(x: T, y: T, z: T) => {
+  return [x, y, z];
+};
+
+console.log(echo4("foo", "biz", "baz"));
 
 class Mirror<T> {
   constructor(public value: T) {}
@@ -24,6 +32,6 @@ class Mirror<T> {
   }
 }
 
-console.log(new Mirror(233).echo());
-console.log(new Mirror("T").echo());
-console.log(new Mirror(true).echo());
+console.log(new Mirror<number>(233).echo());
+console.log(new Mirror<string>("T").echo());
+console.log(new Mirror<boolean>(true).echo());
