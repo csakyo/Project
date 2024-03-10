4c546e9cb26d94380484594dbe6fffab9ee25f0a
csakyo <c.sakyou@gmail.com>
2024-03-10 17:32:58 +0900

    feat: loop_handling

--- React_basic/06_control_and_form/src/010_list_components/Example.js
+++ React_basic/06_control_and_form/src/010_list_components/Example.js
@@ -5,10 +5,14 @@ const Example = () => {
   for (const animal of animals) {
     animalList.push(<li>{animal}</li>);
   }
+  const helloAnimals = animals.map((item) => {
+    return <li>Hello {item}</li>;
+  });
   return (
     <>
       <h3>配列の操作</h3>
       <ul>{animalList}</ul>
+      <ul>{helloAnimals}</ul>
     </>
   );
 };
