1ed1b424416728731d29f7c6a8531a58a8636db2
csakyo <c.sakyou@gmail.com>
2024-03-16 14:09:00 +0900

    feat:filter_arr

--- React_basic/06_control_and_form/src/060_conditional_render/Example.js
+++ React_basic/06_control_and_form/src/060_conditional_render/Example.js
@@ -4,18 +4,22 @@ const Example = () => {
   const animals = ["Dog", "Cat", "Rat"];
   return (
     <>
-      <input
+      {/* <input
         type="text"
         value={filterVal}
         onChange={(e) => setFilterVal(e.target.value)}
       ></input>
       <ul>
-        {animals.filter((animal) => {
-          const isMatcg = animal.indexOf(filterVal) !== -1;
-          console.log(animal.indexOf(filterVal));
-          return isMatch;
-        })}
-      </ul>
+        {animals
+          .filter((animal) => {
+            const isMatcg = animal.indexOf(filterVal) !== -1;
+            console.log(animal.indexOf(filterVal));
+            return isMatch;
+          })
+          .map((animal) => (
+            <li key={animal}>{animal}</li>
+          ))}
+      </ul> */}
     </>
   );
 };
