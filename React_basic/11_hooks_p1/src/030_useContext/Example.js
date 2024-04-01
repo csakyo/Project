605136e741eab503855f072b271811c566eddad7
csakyo <c.sakyou@gmail.com>
2024-04-01 11:41:32 +0900

    feat: useContext

---
+++ React_basic/11_hooks_p1/src/030_useContext/Example.js
@@ -0,0 +1,9 @@
+import Child from "./components/Child";
+import { createContext } from "react";
+export const MyContext = createContext("hello");
+
+const Example = () => {
+  return <Child />;
+};
+
+export default Example;
--- React_basic/11_hooks_p1/src/040_useContext_with_state/Example.js
+++ React_basic/11_hooks_p1/src/040_useContext_with_state/Example.js
@@ -1,10 +1,16 @@
+import { createContext, useState } from "react";
 import Child from "./components/Child";
-import { createContext } from "react";
-export const MyContext = createContext("hello");
+import OtherChild from "./components/OtherChild";
+export const MyContext = createContext();
 
 const Example = () => {
-  const value = "hello";
-  return <Child />;
+  const [state, setState] = useState(0);
+  return (
+    <MyContext.Provider value={[state, setState]}>
+      <Child />
+      <OtherChild />
+    </MyContext.Provider>
+  );
 };
 
 export default Example;
