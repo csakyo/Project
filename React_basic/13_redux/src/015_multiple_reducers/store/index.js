da206ddffd0d9afb7cbc66037800634e1131f7ae
csakyo <c.sakyou@gmail.com>
2024-04-28 09:18:14 +0900

    feat: actionCreater

--- React_basic/13_redux/src/015_multiple_reducers/store/index.js
+++ React_basic/13_redux/src/015_multiple_reducers/store/index.js
@@ -1,16 +1,5 @@
 import { createStore, combineReducers } from "redux";
-
-const initialState = 0;
-const reducer = (state = initialState, { type, step }) => {
-  switch (type) {
-    case "+":
-      return state + step;
-    case "-":
-      return state - step;
-    default:
-      return state;
-  }
-};
+import { reducer } from "./modules/counter";
 
 const reducers = combineReducers({
   conuter: reducer,
