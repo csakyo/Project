ba4764e90183664e702b055815b1809c86c2988f
csakyo <c.sakyou@gmail.com>
2024-04-30 20:01:25 +0900

    feat: toolkit

---
+++ React_basic/13_redux/src/040_immer/components/CounterResult.js
@@ -0,0 +1,8 @@
+import { useSelector } from "react-redux";
+const CounterResult = () => {
+  const state = useSelector((state) => state.counter.count);
+  console.log(state);
+  return <h3>{state}</h3>;
+};
+
+export default CounterResult;
