ee938ccff661a8250f321425fe4f6bd76c95508f
csakyo <c.sakyou@gmail.com>
2024-05-01 16:26:21 +0900

    feat: immer

--- React_basic/13_redux/src/040_immer/Example.js
+++ React_basic/13_redux/src/040_immer/Example.js
@@ -2,6 +2,7 @@ import Counter from "./components/Counter";
 // import { CounterProvider } from "./context/CounterContext";
 import { Provider } from "react-redux";
 import store from "./store";
+import "./immer";
 
 const Example = () => {
   return (
