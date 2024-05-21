e77bf74d49067c2e634e0a6043aa7c4b5291a22b
csakyo <c.sakyou@gmail.com>
2024-05-21 21:09:38 +0900

    feat: getApi

--- React_basic/22_practice/src/components/LikeButton.js
+++ React_basic/22_practice/src/components/LikeButton.js
@@ -1,15 +1,24 @@
 import { useState } from "react";
 
-const LikeButton = () => {
-  const [count, setCount] = useState(999);
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-  return (
-    <span className="likeButton" onClick={handleClick}>
-      いいね！{count}
-    </span>
-  );
+// const LikeButton = () => {
+//   const [count, setCount] = useState(999);
+//   const handleClick = () => {
+//     setCount(count + 1);
+//   };
+//   return (
+//     <span className="likeButton" onClick={handleClick}>
+//       いいね！{count}
+//     </span>
+//   );
+// };
+
+const FetchData = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+    .catch(() => alert("error"));
+
+  return <></>;
 };
 
-export default LikeButton;
+export default FetchData;
