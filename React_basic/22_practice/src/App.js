f349a74339036dc18f6dfead2be8a3e5ce5f66da
csakyo <c.sakyou@gmail.com>
2024-05-27 09:18:07 +0900

    feat: application-weather

--- React_basic/22_practice/src/App.js
+++ React_basic/22_practice/src/App.js
@@ -1,8 +1,8 @@
-import Weather from "./components/Weather";
+import Weather from './components/Weather';
 
 function App() {
   return (
-    <div className="min-h-screen flex justify-center items-center">
+    <div className='min-h-screen flex justify-center items-center'>
       <Weather />
     </div>
   );
--- React_basic/22_practice/src/components/Weather.js
+++ React_basic/22_practice/src/components/Weather.js
@@ -1,7 +1,40 @@
 function Weather() {
   return (
-    <div className="p-4">
-      <div className="bg-gradient-to-r from-blue-500 to-blue-300 w-96 h-56 m-auto bg-blue-500 rounded-xl shadow-2xl transform hover:scale-110 transition-transform "></div>
+    <div className='p-4'>
+      <div
+        className='p-4 bg-gradient-to-r from-blue-500 to-blue-300
+                w-96 m-auto rounded-xl shadow-2xl 
+                transform hover:scale-110 transition-transform
+                text-white'
+      >
+        <div className='flex my-3'>
+          <div className='w-1/2'>
+            <p className='font-light'>City Name</p>
+            <p className='text-lg font-medium tracking-widest'>Tokyo</p>
+          </div>
+          <div className='w-1/2 text-center'>画像</div>
+        </div>
+        <div className='flex my-3'>
+          <div className='w-1/2'>
+            <p className='font-light'>Weather Condition</p>
+            <p className='text-lg font-medium tracking-widest'>Cloudy</p>
+          </div>
+        </div>
+        <div className='flex my-3'>
+          <div className='w-1/3'>
+            <p className='font-light text-xs'>City Name</p>
+            <p className='text-lg font-medium tracking-widest text-sm'>Tokyo</p>
+          </div>
+          <div className='w-1/3'>
+            <p className='font-light text-xs'>City Name</p>
+            <p className='text-lg font-medium tracking-widest text-sm'>Tokyo</p>
+          </div>
+          <div className='w-1/3'>
+            <p className='font-light text-xs'>City Name</p>
+            <p className='text-lg font-medium tracking-widest text-sm'>Tokyo</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
