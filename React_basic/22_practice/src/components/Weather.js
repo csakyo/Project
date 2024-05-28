bf18902940f36de1eb10ac6380e0206cea441745
csakyo <c.sakyou@gmail.com>
2024-05-28 16:21:26 +0900

    feat: application-weather

--- React_basic/22_practice/src/components/Weather.js
+++ React_basic/22_practice/src/components/Weather.js
@@ -1,4 +1,18 @@
+import { useEffect, useState } from 'react';
+import { ENDPOINT, KEY } from '../constants';
+
 function Weather() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`${ENDPOINT}/weather/?q=Tokyo&APPID=${KEY}&units=metric`)
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result);
+        console.log(result);
+      });
+  }, []);
+
   return (
     <div className='p-4'>
       <div
@@ -10,28 +24,32 @@ function Weather() {
         <div className='flex my-3'>
           <div className='w-1/2'>
             <p className='font-light'>City Name</p>
-            <p className='text-lg font-medium tracking-widest'>Tokyo</p>
+            <p className='text-lg font-medium tracking-widest'>{data.name}</p>
           </div>
           <div className='w-1/2 text-center'>画像</div>
         </div>
         <div className='flex my-3'>
           <div className='w-1/2'>
             <p className='font-light'>Weather Condition</p>
-            <p className='text-lg font-medium tracking-widest'>Cloudy</p>
+            <p className='text-lg font-medium tracking-widest'>
+              {data.weather[0].main}
+            </p>
           </div>
         </div>
         <div className='flex my-3'>
           <div className='w-1/3'>
-            <p className='font-light text-xs'>City Name</p>
-            <p className='text-lg font-medium tracking-widest text-sm'>Tokyo</p>
+            <p className='font-light text-xs'>Date</p>
+            <p className='text-lg font-medium tracking-widest text-sm'>
+              2021-03-16
+            </p>
           </div>
           <div className='w-1/3'>
-            <p className='font-light text-xs'>City Name</p>
-            <p className='text-lg font-medium tracking-widest text-sm'>Tokyo</p>
+            <p className='font-light text-xs'>Temprature</p>
+            <p className='text-lg font-medium tracking-widest text-sm'>20℃</p>
           </div>
           <div className='w-1/3'>
-            <p className='font-light text-xs'>City Name</p>
-            <p className='text-lg font-medium tracking-widest text-sm'>Tokyo</p>
+            <p className='font-light text-xs'>Humidity</p>
+            <p className='text-lg font-medium tracking-widest text-sm'>40%</p>
           </div>
         </div>
       </div>
