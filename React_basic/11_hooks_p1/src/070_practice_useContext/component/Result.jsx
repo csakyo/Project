561656d86696ffa872c51c8dd458897cbbe1a665
csakyo <c.sakyou@gmail.com>
2024-06-23 15:11:14 +0900

    feat: practice_useContext

---
+++ React_basic/11_hooks_p1/src/070_practice_useContext/component/Result.jsx
@@ -0,0 +1,12 @@
+import { useCalc, useCalcDispatch } from '../context/CalcContext';
+
+const Result = () => {
+  const state = useCalc();
+  return (
+    <>
+      <h3>結果：{state.result}</h3>
+    </>
+  );
+};
+
+export default Result;
