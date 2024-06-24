e7e693b0b8055ca943d336ae3f9fc0ecd1568692
csakyo <c.sakyou@gmail.com>
2024-06-24 19:42:11 +0900

    feat:practice_useContext

---
+++ React_basic/11_hooks_p1/src/080_practice_reminder/components/Item.jsx
@@ -0,0 +1,12 @@
+const Item = ({ todo, complete }) => {
+  return (
+    <>
+      <div key={todo.id}>
+        <button onClick={() => complete(todo.id)}>完了</button>
+        <span>{todo.content}</span>
+      </div>
+    </>
+  );
+};
+
+export default Item;
---
+++ React_basic/11_hooks_p1/src/080_practice_reminder/components/List.jsx
@@ -0,0 +1,16 @@
+import Item from './Item';
+
+const List = ({ todos, deleteTodo }) => {
+  const complete = (id) => {
+    deleteTodo(id);
+  };
+  return (
+    <div>
+      {todos.map((todo) => (
+        <Item todo={todo} complete={complete} key={todo.id} />
+      ))}
+    </div>
+  );
+};
+
+export default List;
