09808046af5bebec240900e2469d2ab4bf4ebaa3
csakyo <c.sakyou@gmail.com>
2024-06-25 11:04:57 +0900

    feat: practice_reminder

--- React_basic/11_hooks_p1/src/080_practice_reminder/components/Item.jsx
+++ React_basic/11_hooks_p1/src/080_practice_reminder/components/Item.jsx
@@ -1,9 +1,40 @@
-const Item = ({ todo, complete }) => {
+import { useState } from 'react';
+
+const Item = ({ todo, complete, updateTodo }) => {
+  const [editingContent, setEditingContent] = useState(todo.content);
+
+  const changeContent = (e) => setEditingContent(e.target.value);
+  const toggleEditMode = () => {
+    const newTodo = {
+      ...todo,
+      editing: !todo.editing,
+    };
+    updateTodo(newTodo);
+  };
+  const confirmContent = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      ...todo,
+      editing: !todo.editing,
+      content: editingContent,
+    };
+    updateTodo(newTodo);
+  };
   return (
     <>
       <div key={todo.id}>
         <button onClick={() => complete(todo.id)}>完了</button>
-        <span>{todo.content}</span>
+        <form onSubmit={confirmContent} style={{ display: 'inline' }}>
+          {todo.editing ? (
+            <input
+              type='text'
+              value={editingContent}
+              onChange={changeContent}
+            />
+          ) : (
+            <span onDoubleClick={toggleEditMode}>{todo.content}</span>
+          )}
+        </form>
       </div>
     </>
   );
--- React_basic/11_hooks_p1/src/080_practice_reminder/components/List.jsx
+++ React_basic/11_hooks_p1/src/080_practice_reminder/components/List.jsx
@@ -1,13 +1,18 @@
 import Item from './Item';
 
-const List = ({ todos, deleteTodo }) => {
+const List = ({ todos, deleteTodo, updateTodo }) => {
   const complete = (id) => {
     deleteTodo(id);
   };
   return (
     <div>
       {todos.map((todo) => (
-        <Item todo={todo} complete={complete} key={todo.id} />
+        <Item
+          todo={todo}
+          complete={complete}
+          key={todo.id}
+          updateTodo={updateTodo}
+        />
       ))}
     </div>
   );
