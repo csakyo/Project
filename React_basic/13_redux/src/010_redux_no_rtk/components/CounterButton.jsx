2afbe9c9075e6773c7049798ae974321aa6a6367
csakyo <c.sakyou@gmail.com>
2024-07-07 13:40:38 +0900

    feat:Redux

---
+++ React_basic/13_redux/src/010_redux_no_rtk/components/CounterButton.jsx
@@ -0,0 +1,19 @@
+import { useCounterDispatch } from '../context/CounterContext';
+import { useDispatch } from 'react-redux';
+
+const CounterButton = ({ calcType, step }) => {
+  const dispatch = useDispatch();
+  //   const dispatch = useCounterDispatch();
+
+  const clickHandler = () => {
+    dispatch({ type: calcType, step });
+  };
+
+  return (
+    <button onClick={clickHandler}>
+      {calcType}
+      {step}
+    </button>
+  );
+};
+export default CounterButton;
