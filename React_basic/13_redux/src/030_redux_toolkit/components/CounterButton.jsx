4abc579ee80f627ea9659695c216eb1c32021573
csakyo <c.sakyou@gmail.com>
2024-07-21 11:55:53 +0900

    feat: redux_toolkit

--- React_basic/13_redux/src/030_redux_toolkit/components/CounterButton.jsx
+++ React_basic/13_redux/src/030_redux_toolkit/components/CounterButton.jsx
@@ -1,20 +1,16 @@
-import { useDispatch } from 'react-redux';
-import { add, minus } from '../store/modules/counter';
+import { useDispatch } from "react-redux";
+import { add, minus } from "../store/modules/counter"
 
-const CounterButton = ({ calcType, step }) => {
-  const dispatch = useDispatch();
-  console.log(add(step));
+const CounterButton = ({calcType, step}) => {
+    
+    const dispatch = useDispatch();
+    
+    const clickHandler = () => {
+        const action = calcType === '+' ? add(step) : minus(step);
+        console.log(action)
+        dispatch(action);
+    }
 
-  const clickHandler = () => {
-    const action = calcType === '+' ? add(step) : minus(step);
-    dispatch(action);
-  };
-
-  return (
-    <button onClick={clickHandler}>
-      {calcType}
-      {step}
-    </button>
-  );
-};
+    return <button onClick={clickHandler}>{calcType}{step}</button>
+}
 export default CounterButton;
