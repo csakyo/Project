7424d39557af51aa20eba8e1fb182277db202bd0
csakyo <c.sakyou@gmail.com>
2025-01-13 20:28:59 +0900

    feat:link

---
+++ micro_blog/nextjs-microblog-app/app/posts/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Page = () => {
+    return (
+        <div>
+            <h1>最初の投稿</h1>
+            <Link href="/">ホームへ戻る</Link>
+        </div>
+    )
+}
+
+export default Page;
