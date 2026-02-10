8300931f8b3376980b8cc8a8c029ae917df8f89f
csakyo <c.sakyou@gmail.com>
2026-02-10 21:29:06 +0900

    add: page

---
+++ weather/src/app/ooptimistic/page.tsx
@@ -0,0 +1,10 @@
+import { OptimisticCounter } from './OptimisticCounter';
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Optimistic Demo</h1>
+      <OptimisticCounter />
+    </div>
+  );
+}
